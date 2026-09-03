e5332c11f2287cad5593a54f22fb8807bebc55fd
Shruti Ladekar <shrutiladekar@gmail.com>
2026-09-04 02:46:49 +0530

    Fix SQLite database path for Vercel

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./chat.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:////tmp/chat.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
